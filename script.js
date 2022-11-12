b12035e8b0c551c7d8b213744af623938d781227
dhanush <dhanushnindraedu@gmail.com>
2022-11-12 20:30:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const cors = require('cors');
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+
+app.options('*', cors());
+
+app.get('/', (req, res) => {
+  
+  const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287"
+  ;
+
+  let data = 'WOrking'
+  fetch(url,{
+      mode:'cors'
+    })
+    .then(res => res.json())
+    .then(da => {
+      data = da ;
+      console.log(da)
+      res.send(data)
+    });
+
+    
+    });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('server running on port 3000', '');
+});
