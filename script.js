06c2673f166cc85ce23673cc94c723b02efb8e2c
dhanush <dhanushnindraedu@gmail.com>
2022-11-13 14:00:50 +0530

    16h commit

--- script.js
+++ script.js
@@ -8,34 +8,58 @@ app.use(cors({
   credentials: true,
   origin: '*'
 }));
+const axios = require('axios');
+
 
 // app.options('*', cors());
 
 app.get('/', (req, res) => {
   
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.MY_API_KEY,
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});
+  
   const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD"
   ;
 
-  let data = 'WOrking'
+  // let data = 'WOrking'
 
 
-  fetch(url,{headers: { 'X-CMC_PRO_API_KEY':process.env.MY_API_KEY }})
-    .then((response) => {
-      console.log("\\\\THIS IS RESPONSE AFTER FETCHING 20 LISTED JSON CRYPTO FROM COINMARKETCAP API \\\\")
-      console.log(response);
+  // fetch(url,{headers: { 'X-CMC_PRO_API_KEY':process.env.MY_API_KEY }})
+  //   .then((response) => {
+  //     console.log("\\\\THIS IS RESPONSE AFTER FETCHING 20 LISTED JSON CRYPTO FROM COINMARKETCAP API \\\\")
+  //     console.log(response);
       
-      response.json()
+  //     response.json()
       
 
-    })
-    .then((da) => {
-      console.log("\\\\THIS IS RESPONSE CONVERTED INTP JS OBJECT W JSON DATA FETCHED FROM API \\\\")
-      //console.log(da)
-      //data =  JSON.stringify(da);
-      console.log(da)
-      res.json(da);
+  //   })
+  //   .then((da) => {
+  //     console.log("\\\\THIS IS RESPONSE CONVERTED INTP JS OBJECT W JSON DATA FETCHED FROM API \\\\")
+  //     //console.log(da)
+  //     //data =  JSON.stringify(da);
+  //     console.log(da)
+  //     res.json(da);
       
-    }).then(r => res.send());
+  //   }).then(r => res.send());
 
     
     });
