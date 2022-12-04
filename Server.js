d7f29651b12e9501c234405f82b1c24c618cdd22
dhanushnindra <dhanushnindraedu@gmail.com>
2022-12-04 17:15:59 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const app = express();
 const cors = require('cors');
 //require('dotenv').config()
-
+console.log("Deployed athe cde");
 app.use(cors({
   credentials: true,
   origin: '*'
@@ -15,51 +15,51 @@ const axios = require('axios');
 
 app.get('/', (req, res) => {
   
-let response = null;
-new Promise(async (resolve, reject) => {
-  try {
-    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD', {
-      headers: {
-        'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-      },
-    });
-  } catch(ex) {
-    response = null;
-    // error
-    console.log(ex);
-    reject(ex);
-  }
-  if (response) {
-    // success
-    const json = response.data;
-    console.log(json);
-    resolve(json);
-  }
-}).then(js => res.json(js));
+// let response = null;
+// new Promise(async (resolve, reject) => {
+//   try {
+//     response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD', {
+//       headers: {
+//         'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+//       },
+//     });
+//   } catch(ex) {
+//     response = null;
+//     // error
+//     console.log(ex);
+//     reject(ex);
+//   }
+//   if (response) {
+//     // success
+//     const json = response.data;
+//     console.log(json);
+//     resolve(json);
+//   }
+// }).then(js => res.json(js));
   
   const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD"
   ;
 
-  // let data = 'WOrking'
+  let data = 'WOrking'
 
 
-  // fetch(url,{headers: { 'X-CMC_PRO_API_KEY':process.env.MY_API_KEY }})
-  //   .then((response) => {
-  //     console.log("\\\\THIS IS RESPONSE AFTER FETCHING 20 LISTED JSON CRYPTO FROM COINMARKETCAP API \\\\")
-  //     console.log(response);
+  fetch(url,{headers: { 'X-CMC_PRO_API_KEY':process.env.CMC_API_KEY }})
+    .then((response) => {
+      console.log("\\\\THIS IS RESPONSE AFTER FETCHING 20 LISTED JSON CRYPTO FROM COINMARKETCAP API \\\\")
+      console.log(response);
       
-  //     response.json()
+      response.json()
       
 
-  //   })
-  //   .then((da) => {
-  //     console.log("\\\\THIS IS RESPONSE CONVERTED INTP JS OBJECT W JSON DATA FETCHED FROM API \\\\")
-  //     //console.log(da)
-  //     //data =  JSON.stringify(da);
-  //     console.log(da)
-  //     res.json(da);
+    })
+    .then((da) => {
+      console.log("\\\\THIS IS RESPONSE CONVERTED INTP JS OBJECT W JSON DATA FETCHED FROM API \\\\")
+      //console.log(da)
+      //data =  JSON.stringify(da);
+      console.log(da)
+      res.json(da);
       
-  //   }).then(r => res.send());
+    }).then(r => res.send());
 
     
     });
