6e18154d4ee811134548a437767a03ab7383460b
dhanushnindra <dhanushnindraedu@gmail.com>
2023-01-12 20:43:29 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ var app = express();
 app.get('/', function (req, res) {
     fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287')
   .then((response) => response.json())
-  .then((data) => res.send(data));
+  .then((data) => res.json(data));
 
    
    
