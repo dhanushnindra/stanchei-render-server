f3cb47095e7d5a111857d05ca8f7ac3a78ca3794
dhanushnindra <dhanushnindraedu@gmail.com>
2023-01-12 20:07:45 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    fetch('http://example.com/movies.json')
+    fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287')
   .then((response) => response.json())
   .then((data) => console.log(data));
 
@@ -12,8 +12,10 @@ app.get('/', function (req, res) {
 })
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+    
+    console.log("server working");
+//    var host = server.address().address
+//    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+//    console.log("Example app listening at http://%s:%s", host, port)
 })
