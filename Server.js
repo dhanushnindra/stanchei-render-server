00c66325883a386a9e41dfd990c0e50b67b675d6
dhanushnindra <dhanushnindraedu@gmail.com>
2023-01-12 20:00:35 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,71 +1,19 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const app = express();
-const cors = require('cors');
-//require('dotenv').config()
-console.log("Deployed athe cde");
-app.use(cors({
-  credentials: true,
-  origin: '*'
-}));
-const axios = require('axios');
 
+var express = require('express');
+var app = express();
 
-// app.options('*', cors());
+app.get('/', function (req, res) {
+    fetch('http://example.com/movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
-app.get('/', (req, res) => {
-  
-// let response = null;
-// new Promise(async (resolve, reject) => {
-//   try {
-//     response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD', {
-//       headers: {
-//         'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-//       },
-//     });
-//   } catch(ex) {
-//     response = null;
-//     // error
-//     console.log(ex);
-//     reject(ex);
-//   }
-//   if (response) {
-//     // success
-//     const json = response.data;
-//     console.log(json);
-//     resolve(json);
-//   }
-// }).then(js => res.json(js));
-  
-  const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=20&convert=USD"
-  ;
+   
+   
+})
 
-  let data = 'WOrking'
-
-
-  fetch(url,{headers: { 'X-CMC_PRO_API_KEY':process.env.CMC_API_KEY }})
-    .then((response) => {
-      console.log("\\\\THIS IS RESPONSE AFTER FETCHING 20 LISTED JSON CRYPTO FROM COINMARKETCAP API \\\\")
-      console.log(response.body);
-      
-      return res.send(response.body)
-      
-
-    }).catch(e => console.log(e))
-//     .then((da) => {
-//       console.log("\\\\THIS IS RESPONSE CONVERTED INTP JS OBJECT W JSON DATA FETCHED FROM API \\\\")
-//       //console.log(da)
-//       //data =  JSON.stringify(da);
-//       console.log(da)
-//       return da;
-      
-//     }).then(r => res.send(r));
-
-});
- 
-
-
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('server running on port 3000', '');
-});
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
